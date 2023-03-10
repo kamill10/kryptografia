12185ecb63c52342c060556fc3da6fa649e692fd
kamill10 <kam.cieslak@o2.pl>
2023-03-10 12:11:55 +0100

    dupa3

--- Modul/src/main/java/org/example/krypto/Key.java
+++ Modul/src/main/java/org/example/krypto/Key.java
@@ -2,7 +2,7 @@ package org.example.krypto;
 
 import java.security.SecureRandom;
 public class Key {
-    private byte[] keyGenerator() {
+    public  byte[] keyGenerator() {
         SecureRandom secureRandom = new SecureRandom();
         byte[] key = new byte[16];
         secureRandom.nextBytes(key);
--- Widok/src/main/java/com/example/widok/HelloController.java
+++ Widok/src/main/java/com/example/widok/HelloController.java
@@ -2,7 +2,7 @@ package com.example.widok;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import org.example.krypto.AES;
+import org.example.krypto.Key;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
@@ -11,16 +11,10 @@ import java.security.SecureRandom;
 public class HelloController {
     public Button keyGenerator;
     public TextField key;
-    AES  aes = new AES();
+    Key generator = new Key();
 
-    public byte[] keyGenerator() {
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] key = new byte[16];
-        secureRandom.nextBytes(key);
-        return key;
-    }
     public void setKey() {
-        String convert = String.valueOf(new BigInteger(keyGenerator()).abs());
+        String convert = String.valueOf(new BigInteger(generator.keyGenerator()));
         key.setText(convert);
     }
 }
