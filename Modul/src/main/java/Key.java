57e8d222c5219a27ed020fdf9f3e7e721a45b2d9
242425 <242425@edu.p.lodz.pl>
2023-03-10 08:55:38 +0100

    key generator

--- Modul/src/main/java/Key.java
+++ Modul/src/main/java/Key.java
@@ -1,4 +1,10 @@
-import java.math.BigInteger;
-
+import java.security.SecureRandom;
 public class Key {
+    private byte[] keyGenerator() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] key = new byte[16];
+        secureRandom.nextBytes(key);
+
+        return key;
+    }
 }
