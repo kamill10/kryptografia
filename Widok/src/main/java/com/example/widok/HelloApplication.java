78465d8eb6796be210c48868901c1b1d7adb5e08
kamill10 <kam.cieslak@o2.pl>
2023-03-12 18:21:29 +0100

    dypa

--- Widok/src/main/java/com/example/widok/HelloApplication.java
+++ Widok/src/main/java/com/example/widok/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.widok;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.example.krypto.AES;
 import org.example.krypto.Key;
@@ -12,16 +13,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Key key = new Key();
-        byte [] klucz =key.keyGenerator();
-      AES aes = new AES(klucz);
-      byte [] arr = "dsgbhjkjhaaaa".getBytes();
-        String str = new String(arr);
-        System.out.println(str);
-      byte[] zakodowane = aes.divideBytesOn128bitsAndEncode(arr);
-      byte[] tab = aes.decode(zakodowane);
-        String str2 = new String(tab);
-        System.out.println(str2);
+
+
 
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
