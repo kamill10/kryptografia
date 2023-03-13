95825d837a67fde79b31f1b8c780229895f689c1
242425 <242425@edu.p.lodz.pl>
2023-03-13 22:34:44 +0100

    można testowac w FX wywala przy decode

--- Widok/src/main/java/com/example/widok/HelloController.java
+++ Widok/src/main/java/com/example/widok/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.widok;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.example.krypto.AES;
 import org.example.krypto.Key;
 
 import java.util.Arrays;
@@ -10,10 +12,36 @@ import java.util.Arrays;
 public class HelloController {
     public Button keyGenerator;
     public TextField key;
+    public TextArea tekst_zaszyfrowany;
+    public TextArea tekst_jawny;
     Key generator = new Key();
+
+    public String getKey() {
+        return key.getText();
+    }
+
+    public void setKey(TextField key) {
+        this.key = key;
+    }
 
     public void setKey() {
         String convert = Arrays.toString(generator.keyGenerator());
         key.setText(convert);
+    }
+
+    public String getTekst_zaszyfrowany() {
+        return tekst_zaszyfrowany.getText();
+    }
+
+    public String getTekst_jawny() {
+        return tekst_jawny.getText();
+    }
+    public void setEncrypt() {
+        AES aes = new AES(getKey().getBytes());
+        tekst_zaszyfrowany.setText(Arrays.toString(aes.divideBytesOn128bitsAndEncode(getTekst_jawny().getBytes())));
+    }
+    public void setDecrypt() {
+        AES aes = new AES(getKey().getBytes());
+        tekst_jawny.setText(Arrays.toString(aes.decode(tekst_zaszyfrowany.getText().getBytes())));
     }
 }
