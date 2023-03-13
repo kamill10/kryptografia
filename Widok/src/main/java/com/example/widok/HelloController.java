58af2860f3a8b6b0fd7dd602d0d9c70be3d2b040
242425 <242425@edu.p.lodz.pl>
2023-03-13 16:05:46 +0100

    Revert "dypa"
    This reverts commit 78465d8eb6796be210c48868901c1b1d7adb5e08.

--- Widok/src/main/java/com/example/widok/HelloController.java
+++ Widok/src/main/java/com/example/widok/HelloController.java
@@ -1,8 +1,6 @@
 package com.example.widok;
 
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import org.example.krypto.Key;
 
@@ -12,13 +10,9 @@ import java.util.Arrays;
 public class HelloController {
     public Button keyGenerator;
     public TextField key;
-    public TextArea plainText;
-    public TextArea encryptedText;
-
     Key generator = new Key();
 
     public void setKey() {
-        System.out.println(key.getText());
         String convert = Arrays.toString(generator.keyGenerator());
         key.setText(convert);
     }
